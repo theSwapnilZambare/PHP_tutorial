f8101be87ef7fc4f79b49ad2e5330d5ac01bcc19
Swapnil Zambare <swapnil.zambare2@gmail.com>
2022-01-02 20:57:48 +0530

    New update v1

---
+++ php_basic/echo.php
@@ -0,0 +1,22 @@
+<?php
+
+	// To print any statement use echo in php
+
+	/*	
+	echo :
+
+	1. using echo statement we can print the multiple variables with comma separated, where as by using print we can not
+
+	2. print statement will always returns true after printing the output. where as echo statement does not return any value
+	
+	*/
+	
+	
+   echo "Welcome to PHP Tutorial by Swapnil"; // normal line print
+
+   echo "<br>"; // break tag
+
+   echo '<h2>This is Html heading tag h2 example.</h2>'; // html tag used in php echo
+
+   
+?>
