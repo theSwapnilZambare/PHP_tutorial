bdee3b8afd3f13be7d72146c6926331038554f6d
Swapnil Zambare <swapnil.zambare2@gmail.com>
2022-01-04 12:54:33 +0530

    13

---
+++ php_basic/13_states.php
@@ -0,0 +1,40 @@
+<?php
+
+  //if elseif else
+
+/*
+   $age = 18;
+
+   if ($age == 12) {
+   	echo "you should not be here";
+
+   } elseif ($age == 18) {
+   	echo "your 18 your perfectly welcome";
+
+   } else {
+   	echo "you are welcome here";
+   }
+*/
+
+
+  //switch
+
+  $favColor = "black";
+
+  switch ($favColor) {
+
+  	case "black":
+        echo "This color is black";
+        break;
+
+    case "green":
+        echo "This color is green";
+        break;
+
+    default:
+        echo "This another color";        
+  } 
+
+
+
+?>
