53bdd84775ab20911ef95013b34c654328b3493c
Swapnil Zambare <swapnil.zambare2@gmail.com>
2022-01-04 13:08:09 +0530

    Create 16_functions.php

---
+++ php_basic/16_functions.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+   function myFunc() {
+
+   	echo "Hello this is funtion";
+
+   	$username = "PHP Development";
+
+   	if ($username == "PHP Development") {
+   		echo "Hello, PHP Development";
+   	} else {
+   		echo "you are not PHP Developer, should not be here";
+   	}
+   }
+
+   myfunc();
+   
+*/
+
+
+  function printFullName($fname, $lname) {
+    echo "your first name is: $fname and your last name is: $lname";
+  }
+  printFullName("Swapnil", "Test");
+  
+?>
