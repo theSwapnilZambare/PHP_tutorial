81aeb6e8fba2719ecedff2c0c4b876353765a748
Swapnil Zambare <swapnil.zambare2@gmail.com>
2022-01-04 12:58:55 +0530

    14

---
+++ php_basic/14_loops.php
@@ -0,0 +1,77 @@
+<?php 
+
+  //loops
+
+  $i = 20;
+
+   while ($i > 0) {
+   	echo "number is: $i <br>";
+   	$i--;
+   }
+
+   for($i=0; $i<20; $i++) {
+   	echo "value is: $i <br>";
+   }
+
+
+	// do while loop
+/*
+	$i = 15;
+	do{
+		echo $i."<br>";
+		$i++;
+	}
+	while($i <= 10);
+
+*/
+
+	// while loop
+/*
+$i = 15;
+while($i<=10)
+{
+	echo $i."<br>";
+	$i++;
+}
+*/
+
+
+/*
+$i = 1;
+$table = 19;
+while($i <= 10)
+{
+	echo $table."*".$i."=".$table*$i."<br>";
+	$i++;
+}
+*/
+
+	// for loop
+/*
+for($i = 1; $i <= 10; $i++)
+{
+	if($i % 2 == 1)
+	{
+		echo $i."<br>";
+	}
+}
+*/
+
+
+/*for($i = 10; $i >= 1; $i--)
+{
+	echo $i."<br>";
+}*/
+
+
+
+/*for($i = 1; $i <= 10; $i++)
+{
+	//echo $i;
+	//echo "<br>";
+	
+	echo $i."<br>";
+}
+*/
+
+?>
